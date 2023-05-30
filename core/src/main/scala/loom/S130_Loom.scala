bd922fe0279f1b7e7782ba3dca624b3210a80ae0
adamw <adam@warski.org>
2023-05-30 11:10:58 +0200

    WIP

--- core/src/main/scala/loom/S130_Loom.scala
+++ core/src/main/scala/loom/S130_Loom.scala
@@ -5,9 +5,12 @@ import org.slf4j.LoggerFactory
 val log = LoggerFactory.getLogger("loom")
 
 @main def loom(): Unit =
-  for (i <- 1 to 100000)
-    Thread.startVirtualThread(new Runnable {
-      override def run(): Unit = log.info(s"Hello, world! ($i)")
-    })
+  val start = System.currentTimeMillis()
 
-  Thread.sleep(5000)
+  val threads =
+    for (i <- 1 to 100000)
+      yield Thread.startVirtualThread(() => log.info(s"Hello, world! ($i)"))
+
+  threads.foreach(_.join())
+
+  println(s"Took: ${System.currentTimeMillis() - start}ms")
